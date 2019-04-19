7226122734b9d7d3acd4aaf76ac90fc6379165a4
macrosf@ocj <macros_f@163.com>
2019-04-19 18:19:12 +0800

    5-12  带cookie的跨域1

--- src/main/java/com/imooc/ajaxserver/CrosFilter.java
+++ src/main/java/com/imooc/ajaxserver/CrosFilter.java
@@ -22,8 +22,17 @@ public class CrosFilter implements Filter {
 	@Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse res = (HttpServletResponse) servletResponse;
-        res.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
-        res.setHeader("Access-Control-Allow-Methods", "GET");
+
+        //带cookie的时候，orgin必须是全匹配，不能使用*
+        res.addHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+        res.addHeader("Access-Control-Allow-Methods", "GET");
+        //预检命令响应头
+		res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+		//预检命令缓存响应头
+		res.addHeader("Access-Control-Max-Age", "3600");
+
+		//enable cookie
+		res.addHeader("Access-Control-Allow-Credential", "true");
         filterChain.doFilter(servletRequest,servletResponse);
     }
 	@Override
